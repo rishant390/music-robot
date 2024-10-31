2dc6c086fd9f5dd4ae694a7073933199838d4c97
manoranjan23 <manoranjanhor43@gmail.com>
2024-11-01 01:09:18 +0530

    Update play.py

--- ShilpaXMusic/utils/inline/play.py
+++ ShilpaXMusic/utils/inline/play.py
@@ -66,7 +66,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [ InlineKeyboardButton(text="sᴏᴍᴜ࿐", url=f"https://t.me/alone_somu6")],
+        [ InlineKeyboardButton(text="sᴏᴍᴜ࿐", url=f"https://t.me/somueditingzone")],
     ]
     
     return buttons
