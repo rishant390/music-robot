22835276150251610a3238f4edfa9ed8ad33c098
X-Network12 <Shivmis165@gmail.com>
2024-11-06 15:34:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/manoranjan23/ShilpaXMusic",
+    "https://github.com/X-Network12/Network",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
